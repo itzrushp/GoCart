d67c7a29bde1f1a836490d97b4f12088fbe4becc
Rush <itzashert@gmail.com>
2026-01-18 11:43:18 +0530

    Bug Fixes

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,16 +1,18 @@
 import { serve } from "inngest/next";
-import { inngest } from "../../../inngest/client";
-import { syncUserCreation, syncUserDeletion, syncUserUpdation } from "./functions";
+import { inngest } from "./client";
+import {
+  syncUserCreation,
+  syncUserDeletion,
+  syncUserUpdation,
+} from "./functions";
 
 export const runtime = "nodejs";
 
-// Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    /* your functions will be passed here later! */
     syncUserCreation,
     syncUserUpdation,
-    syncUserDeletion
+    syncUserDeletion,
   ],
 });
